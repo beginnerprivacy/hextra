f9ce36b1844c3c65cb877cb4218be2c1af05f637
PrintN <127101769+PrintN@users.noreply.github.com>
2025-01-22 14:08:40 +0100

    Added quiz

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -35,3 +35,64 @@ function updateRoadmap() {
   glider.style.transform = `translateX(${labelRect.left - tabsRect.left}px)`;
 }
 window.onload = updateRoadmap;
+
+let score = 0;
+let currentQuestionIndex = 0;
+
+function takeQuiz() {
+    score = 0;
+    currentQuestionIndex = 0;
+    document.getElementById('quizModal').style.display = 'block';
+    showQuestion();
+}
+
+function showQuestion() {
+    const quizResult = document.getElementById('quizResult');
+    quizResult.style.display = 'none';
+    const questions = document.querySelectorAll('.question');
+    questions.forEach(question => question.style.display = 'none');
+    
+    const quizButtons = document.querySelectorAll('.yes-no-button');
+    
+    if (currentQuestionIndex === 2 || currentQuestionIndex === 3) {
+        quizButtons.forEach(button => button.style.display = 'none');
+    } else {
+        quizButtons.forEach(button => button.style.display = 'inline');
+    }
+
+    if (currentQuestionIndex < questions.length) {
+        questions[currentQuestionIndex].style.display = 'block';
+    } else {
+        determineRoadmap();
+    }
+}
+
+function answerQuestion(answer) {
+    if (answer === 'yes') {
+        score += 1;
+    }
+    if (currentQuestionIndex === 2 || currentQuestionIndex === 3) {
+        score += answer;
+    }
+    currentQuestionIndex++;
+    showQuestion();
+}
+
+function determineRoadmap() {
+    let roadmapMessage;
+    if (score <= 2) {
+        roadmapMessage = "You should start on the Basic roadmap.";
+    } else if (score <= 4) {
+        roadmapMessage = "You should start on the Medium roadmap.";
+    } else {
+        roadmapMessage = "You should start on the Advanced roadmap.";
+    }
+    document.getElementById('quizResult').innerText = roadmapMessage;
+    quizResult.style.display = 'block';
+    
+    const questions = document.querySelectorAll('.question');
+    questions.forEach(question => question.style.display = 'none');
+    
+    const quizButtons = document.querySelectorAll('.yes-no-button');
+    quizButtons.forEach(button => button.style.display = 'none');
+}
