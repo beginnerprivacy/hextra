16122f7b892ce8a5e1793f2f2c07a1a55e3fb104
PrintN <127101769+PrintN@users.noreply.github.com>
2025-01-27 20:06:34 +0100

    Added share functionality

--- assets/js/tabs.js
+++ assets/js/tabs.js
@@ -95,3 +95,20 @@ function scrollDown () {
         behavior: 'smooth'
     });
 };
+
+function toggleShareDropdown () {
+  const dropdown = document.getElementById("shareDropdown");
+  dropdown.style.display = "block";
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.share-button')) {
+        const dropdowns = document.getElementsByClassName ("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+            const openDropdown = dropdowns[i];
+            if (openDropdown.style.display === "block") {
+                openDropdown.style.display = "none";
+            }
+        }
+    }
+}
