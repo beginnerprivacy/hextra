2a428bfe22fac027c0ffba7743e39e5f15f52d13
yiyi <127101769+PrintN@users.noreply.github.com>
2025-03-28 20:38:15 +0100

    chore: Update homepage

--- assets/js/misc.js
+++ assets/js/misc.js
@@ -14,3 +14,43 @@ function toggleShareDropdown () {
           }
       }
   }
+
+
+  let currentIndex = 0;
+
+  function getItemsToShow() {
+      const width = window.innerWidth;
+  
+      if (width >= 1200) {
+          return 2.7;
+      } else if (width >= 992) {
+          return 2;
+      } else if (width >= 768) {
+          return 1.9;
+      } else {
+          return 1.4;
+      }
+  }
+  
+  function moveCarousel(direction) {
+      const items = document.querySelectorAll('.carousel-item');
+      const totalItems = items.length;
+      const itemsToShow = getItemsToShow();
+  
+      currentIndex += direction;
+  
+      if (currentIndex < 0) {
+          currentIndex = Math.ceil(totalItems / itemsToShow) - 1;
+      } else if (currentIndex >= Math.ceil(totalItems / itemsToShow)) {
+          currentIndex = 0;
+      }
+  
+      const track = document.querySelector('.carousel-track');
+      const offset = -currentIndex * (100 / itemsToShow);
+      track.style.transform = `translateX(${offset}%)`;
+  }
+  
+  window.addEventListener('resize', () => {
+      currentIndex = 0;
+      moveCarousel(0);
+  });
